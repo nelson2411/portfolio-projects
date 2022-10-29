6779335b69f06fa58638df11fd03357452412f36
Nelson Rosales <nelson.rosales83@gmail.com>
2022-10-29 13:00:12 +0200

    feat: error page & contact page

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,7 +1,30 @@
 import React from "react"
 import socialLinks from "../constants/social_links"
 const Footer = () => {
-  return <h2>footer component</h2>
+  return (
+    <footer className="footer">
+      <h4>Created by Nelson Rosales</h4>
+      <p>© {new Date().getFullYear()} All rights reserved</p>
+
+      <div>
+        <div className="footer-links social-links">
+          {socialLinks.map((link, index) => {
+            return (
+              <a
+                className="social-link"
+                key={index}
+                href={link.url}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {link.icon}
+              </a>
+            )
+          })}
+        </div>
+      </div>
+    </footer>
+  )
 }
 
 export default Footer
