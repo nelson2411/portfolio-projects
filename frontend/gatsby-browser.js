d9529ba6f5c19848331b6298cdfe54cc12200be3
Nelson Rosales <nelson.rosales83@gmail.com>
2022-11-01 12:55:28 +0100

    fix: deploy build env

--- frontend/gatsby-browser.js
+++ frontend/gatsby-browser.js
@@ -1,5 +1,5 @@
 const React = require("react")
-const Layout = require("./src/components/layout").default
+const Layout = require("./src/components/Layout").default
 
 exports.wrapPageElement = ({ element, props }) => {
   return <Layout {...props}>{element}</Layout>
