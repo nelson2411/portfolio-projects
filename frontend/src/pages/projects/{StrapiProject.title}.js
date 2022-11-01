617c7d63b94157b2b4821d52c73b59fd92e8ee24
Nelson Rosales <nelson.rosales83@gmail.com>
2022-11-01 14:05:16 +0100

    fix: SEO build

--- frontend/src/pages/projects/{StrapiProject.title}.js
+++ frontend/src/pages/projects/{StrapiProject.title}.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { graphql } from "gatsby"
-import SEO from "../../components/SEO"
+import SEO from "../../components/Seo"
 
 const ProjectTemplate = ({ pageContext: { title }, data }) => {
   return (
