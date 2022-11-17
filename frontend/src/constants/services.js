dccbfff234bcb579eeae5802a642a36be08f07f5
Nelson Rosales <nelson.rosales83@gmail.com>
2022-11-17 08:19:08 +0100

    feat: services updated

--- frontend/src/constants/services.js
+++ frontend/src/constants/services.js
@@ -5,19 +5,21 @@ const services = [
     id: 1,
     icon: <FaCode className="service-icon" />,
     title: "web development",
-    text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
+    text: `My experience includes building websites for small businesses in a variety of industries. Bring your ideas to life with HTML/CSS and Javascript. With me, you'll get a complete solution to your project, from domain management to coding to hosting. .`,
   },
   {
     id: 2,
     icon: <FaSketch className="service-icon" />,
-    title: "web design",
-    text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
+    title: "Analytics",
+    text: `Would you like to know how to tailor your marketing strategies? Using Google analytics, I can provide insights into what your audience is looking for on your website. Additionally, organic SEO is included. 
+.`,
   },
   {
     id: 3,
     icon: <FaAndroid className="service-icon" />,
-    title: "app design",
-    text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
+    title: "React Web-Apps",
+    text: `Are you looking for a ReactJs developer to join your team. Feel free to contact me. My experience included React 16+, NextJs and Gatsby. 
+    I am confortable working with SASS, Styled-components and Material UI.`,
   },
 ]
 
